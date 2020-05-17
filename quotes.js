d0966ff5ea85c60345c7ccd09a79950b909d80b4
tony <chcjswo@gmail.com>
2020-05-17 14:19:48 +0900

    수정: 인용문 받아오는 api 변경 인용문 api mesh -> rapid api로 변경

--- quotes.js
+++ quotes.js
@@ -2,15 +2,17 @@ const got = require('got');
 
 (async () => {
     const client = got.extend({
-        baseUrl: 'https://andruxnet-random-famous-quotes.p.mashape.com',
+        baseUrl: 'https://andruxnet-random-famous-quotes.p.rapidapi.com/',
         headers: {
-            'X-Mashape-Key': process.env.MashapeKey,
-            Accept: 'application/json',
+            'x-rapidapi-host': 'andruxnet-random-famous-quotes.p.rapidapi.com',
+            'x-rapidapi-key': process.env.rapidapiKey,
+            'content-type': 'application/x-www-form-urlencoded',
+            useQueryString: true
         },
     });
 
     // quotes 사이트 호출하고 메시지 받기
     const data = await client.get('/?cat=famous&count=1');
 
-    console.log(data);
+    console.log(data.body);
 })();
